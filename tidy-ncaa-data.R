b3e32d360a9e66820ea321529edb334d93bd1c01
Julia Moody <moodyj20@mail.wlu.edu>
2019-10-24 13:08:59 -0400

    gathered apr rate columns by year

--- tidy-ncaa-data.R
+++ tidy-ncaa-data.R
@@ -11,13 +11,8 @@ names(ncaa_data) <- tolower(names(ncaa_data))
 
 #rearrange columns alphabetically
 ncaa_data <- ncaa_data[ , order(names(ncaa_data))]
-  
 
 #gather columns
-ncaa_data <- gather(ncaa_data[83:96], key = "year_award", value = "award", PUB_AWARD_19:PUB_AWARD_06)
+ncaa_data <- gather(ncaa_data, key = "year_apr_rate", value = "apr_rate", apr_rate_2004_1000:apr_rate_2018_1000)
 
-pub_awards <- ncaa_data %>%
-  select(PUB_AWARD_06, PUB_AWARD_07, PUB_AWARD_08, PUB_AWARD_09, PUB_AWARD_10, PUB_AWARD_11, PUB_AWARD_12, PUB_AWARD_13, PUB_AWARD_14, PUB_AWARD_15, PUB_AWARD_16, PUB_AWARD_17, PUB_AWARD_18, PUB_AWARD_19)
-
-pub_awards_gathered <- gather(pub_awards, key = "year", value = "award", PUB_AWARD_06:PUB_AWARD_19)
 
