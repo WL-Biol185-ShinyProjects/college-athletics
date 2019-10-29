cd28e2afb1522dca0c7ad5ce015d01b9789ab442
Kyce Darouiche <darouichek20@mail.wlu.edu>
2019-10-29 11:59:27 -0400

    Gathering year eligibility rate and put description on the year_apr_rate gather

--- tidy-ncaa-data.R
+++ tidy-ncaa-data.R
@@ -13,6 +13,12 @@ names(ncaa_data) <- tolower(names(ncaa_data))
 ncaa_data <- ncaa_data[ , order(names(ncaa_data))]
 
 #gather columns
+#year_apr_rate
 ncaa_data <- gather(ncaa_data, key = "year_apr_rate", value = "apr_rate", apr_rate_2004_1000:apr_rate_2018_1000)
+
+#year_elig_rate
+ncaa_data <- gather(ncaa_data, key= "year_elig_rate", value= "elig_rate", elig_rate_2004:elig_rate_2018)
+
+
 
 
