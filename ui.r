be044e8a9bc8b60c094ddeca989010d379f8b809
Kyce Darouiche <darouichek20@mail.wlu.edu>
2019-12-02 17:58:10 -0500

    Created a dashboard with tabs. Still need to fix size of boxes/formatting

--- ui.r
+++ ui.r
@@ -1,68 +1,142 @@
+# library(shiny)
+# 
+# 
+# 
+# multiyr_ncaa <- read.csv ("multiyr_ncaa.csv")
+# 
+# fluidPage(
+# 
+#   title= "APR Made Interactive",
+#   tabsetPanel(
+#     tabPanel (title = "Density Distribution",
+# 
+#   sidebarLayout(
+# 
+#     sidebarPanel(
+#       selectInput(
+#         inputId = "aprGroupBy",
+#         label = "Group by",
+#         choices = list(Conference= "confname_18",
+#                        School= "scl_name"
+#         ),
+#         selected = "confname_18",
+# 
+#       ),
+#       selectizeInput(
+#         inputId = "aprIncludeSchools",
+#         label = "Schools",
+#         choices = unique(multiyr_ncaa$scl_name),
+#         multiple=TRUE,
+#         selected=unique(multiyr_ncaa$scl_name[1]),
+#       )
+#     ),
+#     mainPanel(
+#       plotOutput("aprDensity")
+#     )
+#   )
+# ),
+# 
+# tabPanel(title = "Box Plots Showing APR",
+# 
+#          sidebarLayout(
+#            sidebarPanel(
+#              selectInput(
+#                inputId = "aprGroupBy",
+#                label = "Group by",
+#                choices = list(Sport = "sport_name",
+#                               Conference = "confname_18",
+#                               School = "scl_name"
+#                               )),
+#                selected = "sport_name",
+#                uiOutput('plotFilter')
+#              ),
+#            mainPanel(
+#              plotOutput("aprBoxPlot"))
+#            )
+# 
+#          )
+# )
+# 
+# 
+#          )
+  
+
+
 library(shiny)
-
-
-
+library(shinydashboard)
 multiyr_ncaa <- read.csv ("multiyr_ncaa.csv")
 
-fluidPage(
-
-  title= "APR Made Interactive",
-  tabsetPanel(
-    tabPanel (title = "Density Distribution",
-
-  sidebarLayout(
-
-    sidebarPanel(
-      selectInput(
-        inputId = "aprGroupBy",
-        label = "Group by",
-        choices = list(Conference= "confname_18",
-                       School= "scl_name"
-        ),
-        selected = "confname_18",
-
-      ),
-      selectizeInput(
-        inputId = "aprIncludeSchools",
-        label = "Schools",
-        choices = unique(multiyr_ncaa$scl_name),
-        multiple=TRUE,
-        selected=unique(multiyr_ncaa$scl_name[1]),
-      )
-    ),
-    mainPanel(
-      plotOutput("aprDensity")
+dashboardPage(
+  dashboardHeader(title="APR Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("density", tabName = "density"),
+      menuItem("boxplots", tabName = "boxplots")
     )
-  )
-),
-
-tabPanel(title = "Box Plots Showing APR",
-
-         sidebarLayout(
-           sidebarPanel(
-             selectInput(
-               inputId = "aprGroupBy",
-               label = "Group by",
-               choices = list(Sport = "sport_name",
-                              Conference = "confname_18",
-                              School = "scl_name"
-                              )),
-               selected = "sport_name",
-               uiOutput('plotFilter')
-             ),
-           mainPanel(
-             plotOutput("aprBoxPlot"))
-           )
-<<<<<<< HEAD
-         ))
-
-=======
-         )
+  ),
   
->>>>>>> 533ae7bd358527b7cf9c5f7072d85c124f9dbd73
+  dashboardBody(
+    tabItems(
+      #First tab content
+      tabItem(tabName = "density",
+              fluidRow(
+                box(
+                  title = "Density",
+                  sidebarLayout(
+                    
+                    sidebarPanel(
+                      selectInput(
+                        inputId = "aprGroupBy",
+                        label = "Group by",
+                        choices = list(Conference= "confname_18",
+                                       School= "scl_name"
+                        ),
+                        selected = "confname_18",
+                        
+                      ),
+                      selectizeInput(
+                        inputId = "aprIncludeSchools",
+                        label = "Schools",
+                        choices = unique(multiyr_ncaa$scl_name),
+                        multiple=TRUE,
+                        selected=unique(multiyr_ncaa$scl_name[1]),
+                      )
+                    ),
+                    mainPanel(
+                      plotOutput("aprDensity")
+                    )
+                  )
+                )
+              )
+      ),
+      #second tab
+      tabItem(tabName = "boxplots",
+              fluidRow(
+                box(
+                  title = "Box Plots Showing APR",
+                  
+                  sidebarLayout(
+                    sidebarPanel(
+                      selectInput(
+                        inputId = "aprGroupBy",
+                        label = "Group by",
+                        choices = list(Sport = "sport_name",
+                                       Conference = "confname_18",
+                                       School = "scl_name"
+                        )),
+                      selected = "sport_name",
+                      uiOutput('plotFilter')
+                    ),
+                    mainPanel(
+                      plotOutput("aprBoxPlot"))
+                  )
+                  
+                )
+              )      
+      )
+    )
+    
+  )
 )
-)
-
-
 
 
