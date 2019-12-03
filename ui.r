ae1c9f3f23c13b6b71a4385237da66250fe25404
Kyce Darouiche <darouichek20@mail.wlu.edu>
2019-12-03 12:48:18 -0500

    Added tabs and text

--- ui.r
+++ ui.r
@@ -70,16 +70,26 @@ dashboardPage(
   dashboardHeader(title="APR Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("density", tabName = "density"),
-      menuItem("boxplots", tabName = "boxplots")
+      menuItem("Intro", tabName = "intro"),
+      menuItem("Density", tabName = "density"),
+      menuItem("Boxplots", tabName = "boxplots"),
+      menuItem("Leaflet", tabName = "leaflet")
     )
   ),
   
   dashboardBody(
     tabItems(
-      #First tab content
+      tabItem(tabName = "intro",
+              fluidRow(
+               p("Implemented by the NCAA, the Academic Progress Rate (APR) is a team-based metric that holds institutions accountable for the academic progress of their student-athletes and accounts for eligibility and retention of each individual student-athlete for each academic term. This measurement system rewards teams with superior academic performance and penalizes teams that do not achieve certain benchmarks."),
+               p("APR is an important measurement to consider for any prospective student athletes who are looking to play for not only a specific school but also a specific team. According to the NCAA, less than 2 percent of NCAA student-athletes become professional athletes. Because of this, student-athletes may wish to emphasize academic performance to prepare them for life after college. "),
+               p("The creators of this application wish to give any prospective student athlete an inside view of the academic performance of a specific school, team, and conference as well as the factors which may contribute to the APR. With this knowledge, one should be able to gain a greater perspective of their lives as student athletes off the field and inside the classroom. This application consists of a density plot and multiple box plots regarding APR and multiple variables as well as a heat map demonstrating the regional differences in APR.")
+              )
+              ),
+     #first tab
       tabItem(tabName = "density",
               fluidRow(
+                p("This is a density plot which shows the distribution of Academic Performance Rates (APR). Choose to group by either school or conference and find out the density of the APR of that particular variable:"),
                 box(
                   width= 12,
                   title = "Density",
@@ -113,6 +123,7 @@ dashboardPage(
       #second tab
       tabItem(tabName = "boxplots",
               fluidRow(
+                p("Choose a sport, conference, or school you are interested in looking at to see a box plot of the Academic Progress Rate (APR). This plot allows you to see the distribution of..."),
                 box(
                   title = "Box Plots Showing APR",
                   width= 12,
@@ -134,7 +145,12 @@ dashboardPage(
                   
                 )
               )      
-      )
+      ),
+     tabItem(tabName= "leaflet",
+             fluidRow(
+               p("View how APR is distributed regionally:")
+             )
+             )
     )
     
   )
