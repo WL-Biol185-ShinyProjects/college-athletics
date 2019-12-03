1b8419153317922af2196810154b76c73b081dd6
Julia Moody <moodyj20@mail.wlu.edu>
2019-12-03 13:13:25 -0500

    added if statement to filter

--- server.r
+++ server.r
@@ -16,19 +16,25 @@ function(input, output){
   output$plotFilter <- renderUI({
     if(input$aprGroup == 'sport_name') {
       selectInput('sport_name', "Sport:", choices = unique(multiyr_ncaa$sport_name))
-      #print("I should be in Sport")
     } else if(input$aprGroup == 'confname_18') {
       selectInput('confname_18', "Conference:", choices = unique(multiyr_ncaa$confname_18))
-      #print("I should be in conference")
     } else {
       selectInput('scl_name', "School:", choices = unique(multiyr_ncaa$scl_name))
-      #print("I should be in school")
     }
   })
   
   output$aprBoxPlot <- renderPlot({
     multiyr_ncaa %>%
-      #filter(aprGroupBy %in% input$aprInclude) %>%
+      filter(
+        if(input$aprGroup == 'sport_name') {
+          selectInput('sport_name', "Sport:", choices = unique(multiyr_ncaa$sport_name))
+        } else if(input$aprGroup == 'confname_18') {
+          selectInput('confname_18', "Conference:", choices = unique(multiyr_ncaa$confname_18))
+        } else {
+          selectInput('scl_name', "School:", choices = unique(multiyr_ncaa$scl_name))
+        }
+        
+      ) %>%
       ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
       geom_boxplot()
   })
