8a18a51e9fe1f96da14361e0997d3622bb4f9a4f
Julia Moody <moodyj20@mail.wlu.edu>
2019-12-05 15:09:16 -0500

    added selected part to renderUI code

--- server.r
+++ server.r
@@ -15,30 +15,36 @@ function(input, output){
   
   output$plotFilter <- renderUI({
     if(input$aprGroup == 'sport_name') {
-      selectInput('sport_name', "Sport:", choices = unique(multiyr_ncaa$sport_name))
+      selectInput('sport_name', "Sport:", choices = unique(multiyr_ncaa$sport_name), selected=unique(multiyr_ncaa$sport_name[1]))
     } else if(input$aprGroup == 'confname_18') {
-      selectInput('confname_18', "Conference:", choices = unique(multiyr_ncaa$confname_18))
+      selectInput('confname_18', "Conference:", choices = unique(multiyr_ncaa$confname_18), selected=unique(multiyr_ncaa$confname_18[1]))
     } else {
-      selectInput('scl_name', "School:", choices = unique(multiyr_ncaa$scl_name))
+      selectInput('scl_name', "School:", choices = unique(multiyr_ncaa$scl_name), selected=unique(multiyr_ncaa$scl_name[1]))
     }
   })
   
   output$aprBoxPlot <- renderPlot({
-    multiyr_ncaa %>%
-      filter( 
-        input$aprGroup %in%
-        if(input$aprGroup == 'sport_name') {
-          unique(multiyr_ncaa$sport_name)
-        } else if(input$aprGroup == 'confname_18') {
-          unique(multiyr_ncaa$confname_18)
-        } else {
-          unque(multiyr_ncaa$scl_name)
-        } 
-
-      ) %>%
-      ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
-      geom_boxplot()
-  })
+    
+    if(input$aprGroup == 'sport_name') {
+      multiyr_ncaa %>%
+        filter('sport_name' %in% input$aprGroup) %>%
+        ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
+        geom_boxplot() 
+    }else if(input$aprGroup == 'confname_18') {
+      multiyr_ncaa %>%
+        filter('confname_18' %in% input$aprGroup) %>%
+        ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
+        geom_boxplot()
+    }else {
+      multiyr_ncaa %>%
+        filter('scl_name' %in% input$aprGroup) %>%
+        ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
+        geom_boxplot()
+    }
+  }
+  
+  )
   
 }
+
 
