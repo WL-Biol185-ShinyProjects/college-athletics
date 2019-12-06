3bf34e7e937daa83371bf8b17cd76f2242264e6a
Julia Moody <moodyj20@mail.wlu.edu>
2019-12-05 20:59:37 -0500

    changed axis and legend labels on density plot

--- server.r
+++ server.r
@@ -9,7 +9,11 @@ function(input, output){
     multiyr_ncaa %>%
       filter(scl_name %in% input$aprIncludeSchools) %>%
       ggplot (aes_string("multiyr_apr_rate_1000_official", fill = input$aprGroupBy)) + 
-      geom_density(alpha=0.2)
+      geom_density(alpha=0.2) +
+      xlab("Academic Performance Rate") +
+      labs(fill = "Name:") +
+      theme(legend.background = element_rect(fill="lightgrey", 
+                                             size=0.5, linetype="solid"))
     
   })
   
@@ -27,19 +31,19 @@ function(input, output){
     
     if(input$aprGroup == 'sport_name') {
       multiyr_ncaa %>%
-        filter('sport_name' %in% input$aprGroup) %>%
+        filter(input$aprGroup %in% unique(sport_name)) %>%
         ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
         geom_boxplot() 
-    }else if(input$aprGroup == 'confname_18') {
-      multiyr_ncaa %>%
-        filter('confname_18' %in% input$aprGroup) %>%
-        ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
-        geom_boxplot()
-    }else {
-      multiyr_ncaa %>%
-        filter('scl_name' %in% input$aprGroup) %>%
-        ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
-        geom_boxplot()
+    # }else if(input$aprGroup == 'confname_18') {
+    #   multiyr_ncaa %>%
+    #     filter('confname_18' %in% input$aprGroup) %>%
+    #     ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
+    #     geom_boxplot()
+    # }else {
+    #   multiyr_ncaa %>%
+    #     filter('scl_name' %in% input$aprGroup) %>%
+        # ggplot(aes_string(x=input$aprGroup, y="multiyr_apr_rate_1000_official")) +
+        # geom_boxplot()
     }
   }
   
